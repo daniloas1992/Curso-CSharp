afbf73fd0c316769d7320f3f47f7b81db4d52b7a
Danilo A. Santos <danilo_as1992@hotmail.com>
2021-01-12 20:26:41 -0300

    DateTime Properties

---
+++ DataHora/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Globalization;
+
+namespace DataHora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime d1 = DateTime.Now;
+            Console.WriteLine(d1);
+            Console.WriteLine(d1.Ticks);
+
+            DateTime d2 = new DateTime(2020, 01, 25); // aaaa/mm/dd
+            DateTime d3 = new DateTime(2020, 01, 25, 20, 45, 3); // aaaa/mm/dd hh:MM:ss
+            DateTime d4 = new DateTime(2020, 01, 25, 20, 45, 3, 500); // aaaa/mm/dd hh:MM:ss:mils
+
+            Console.WriteLine(d2);
+            Console.WriteLine(d3);
+            Console.WriteLine(d4);
+
+            DateTime d5 = DateTime.Today; // aaaa/mm/dd 00:00:00
+
+            Console.WriteLine(d5);
+
+            DateTime d6 = DateTime.UtcNow; // Horário UTC
+
+            Console.WriteLine(d6);
+
+            DateTime d7 = DateTime.Parse("2021-01-20");
+            DateTime d8 = DateTime.Parse("2021-01-20 19:47:25");
+
+            Console.WriteLine(d7);
+            Console.WriteLine(d8);
+
+            DateTime d9  = DateTime.Parse("21/01/2021");
+            DateTime d10 = DateTime.Parse("21/01/2021 19:47:25");
+            DateTime d11 = DateTime.Parse("04/04/2021 19:47:25");
+
+            Console.WriteLine(d9);
+            Console.WriteLine(d10);
+            Console.WriteLine(d11);
+
+            DateTime d12 = DateTime.ParseExact("2021-01-25", "yyyy-MM-dd", CultureInfo.InvariantCulture); //Máscara de data
+
+            Console.WriteLine(d12);
+
+            DateTime d13 = DateTime.ParseExact("25/01/2021 19:47:18", "dd/MM/yyyy HH:mm:ss", CultureInfo.InvariantCulture); //Máscara de data
+
+            Console.WriteLine(d13);
+
+        }
+    }
+}
+
---
+++ TimeSpan/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace TempoSpan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TimeSpan t1 = new TimeSpan(0, 1, 30); // hora, minuto e segundo
+            Console.WriteLine(t1);
+            Console.WriteLine(t1.Ticks);
+
+            TimeSpan t2 = new TimeSpan(900000000L); // ticks, 100 nanosegundos
+            TimeSpan t3 = new TimeSpan();
+
+            Console.WriteLine(t2);
+            Console.WriteLine(t3);
+
+            TimeSpan t4 = new TimeSpan(2, 0, 1, 30); // dias, hora, minuto e segundo
+            Console.WriteLine(t4);
+
+            TimeSpan t5 = new TimeSpan(2, 0, 1, 30, 500); // dias, hora, minuto, segundo, milisegundos
+            Console.WriteLine(t5);
+
+            TimeSpan t6 = TimeSpan.FromDays(1.5);
+            TimeSpan t7 = TimeSpan.FromHours(1.5);
+            TimeSpan t8 = TimeSpan.FromMinutes(1.5);
+            TimeSpan t9 = TimeSpan.FromSeconds(1.5);
+            TimeSpan t10 = TimeSpan.FromMilliseconds(1.5);
+            TimeSpan t11 = TimeSpan.FromTicks(900000000L);
+
+            Console.WriteLine(t6);
+            Console.WriteLine(t7);
+            Console.WriteLine(t8);
+            Console.WriteLine(t9);
+            Console.WriteLine(t10);
+            Console.WriteLine(t11);
+
+        }
+    }
+}
